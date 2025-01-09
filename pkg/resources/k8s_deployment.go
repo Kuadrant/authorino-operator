a3ebcd329b3a6ddbcedce83678d6be1cc5fbdbca
bartoszmajsak <bartosz.majsak@gmail.com>
2025-01-09 19:07:10 +0100

    fix(labels): propagates labels to Deployment's spec.template
    PR #91 brought the ability to propagate labels defined for Authorino CR
    down to Deployment. However, more often than not, it's more desired to
    have those labels propagated to template and therefore pods which are
    part of the deployment. This can simplify e.g. making Authorino part of
    service mesh, as injection label can be consistently added to relevant
    resources.
    
    When creating the deployment, instead of having fixed set of labels for
    Deployment's `spec.template` this change appends Authorino CR labels as
    well.
    
    Signed-off-by: bartoszmajsak <bartosz.majsak@gmail.com>

--- pkg/resources/k8s_deployment.go
+++ pkg/resources/k8s_deployment.go
@@ -1,25 +1,36 @@
 package resources
 
 import (
+	"github.com/go-logr/logr"
 	k8sapps "k8s.io/api/apps/v1"
 	k8score "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func GetDeployment(name, namespace, saName string, replicas *int32, containers []k8score.Container, vol []k8score.Volume, labels map[string]string) *k8sapps.Deployment {
-	objMeta := getObjectMeta(namespace, name, labels)
-	authorinoLabels := labelsForAuthorino(name)
+func GetDeployment(name, namespace, saName string, replicas *int32, containers []k8score.Container, vol []k8score.Volume, labelsFromAuthorinoCR map[string]string, logger logr.Logger) *k8sapps.Deployment {
+	mutableLabels := defaultAuthorinoLabels(name)
+	if labelsFromAuthorinoCR != nil {
+		for key, value := range labelsFromAuthorinoCR {
+			if key == "limitador-resource" || key == "app" {
+				logger.V(1).Info("skipping authorino labels with keys \"control-plane\" and \"authorino-resource\" as these are reserved for use by the operator")
+				continue
+			}
+			mutableLabels[key] = value
+		}
+	}
+	immutableLabels := defaultAuthorinoLabels(name)
+	objMeta := getObjectMeta(namespace, name, mutableLabels)
 
 	return &k8sapps.Deployment{
 		ObjectMeta: objMeta,
 		Spec: k8sapps.DeploymentSpec{
 			Replicas: replicas,
 			Selector: &v1.LabelSelector{
-				MatchLabels: authorinoLabels,
+				MatchLabels: immutableLabels,
 			},
 			Template: k8score.PodTemplateSpec{
 				ObjectMeta: v1.ObjectMeta{
-					Labels: authorinoLabels,
+					Labels: mutableLabels,
 				},
 				Spec: k8score.PodSpec{
 					ServiceAccountName: saName,
@@ -74,4 +85,17 @@ func GetTlsVolume(certName, secretName string) k8score.Volume {
 			},
 		},
 	}
+}
+
+func MapUpdateNeeded(existing map[string]string, desired map[string]string) bool {
+	if existing == nil {
+		existing = map[string]string{}
+	}
+
+	for k, v := range desired {
+		if existingVal, exists := (existing)[k]; !exists || v != existingVal {
+			return true
+		}
+	}
+	return false
 }
